6f84130c3da9c02ed8df01c4557bf48526679942
burncan <burncan@proton.me>
2025-06-07 12:17:44 -0400

    first commit

---
+++ build/CMakeFiles/OpenGLApp.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/backends/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/backends/imgui_impl_glfw.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/backends/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/backends/imgui_impl_opengl3.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_demo.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_demo.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_draw.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_draw.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_tables.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_tables.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_widgets.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/_deps/imgui-src/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/src/main.cpp.obj.d"
+  "CMakeFiles/OpenGLApp.dir/src/shader_loader.cpp.obj"
+  "CMakeFiles/OpenGLApp.dir/src/shader_loader.cpp.obj.d"
+  "bin/OpenGLApp.exe"
+  "bin/OpenGLApp.exe.manifest"
+  "bin/OpenGLApp.pdb"
+  "libOpenGLApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
